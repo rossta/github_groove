3f787e3e7fb56b48bde291a004ed897333a59f41
Ross Kaffenberger <rosskaff@gmail.com>
2016-02-15 09:16:38 -0500

    disable hash

--- apps/web/application.rb
+++ apps/web/application.rb
@@ -277,7 +277,7 @@ module Web
         # Use digest file name for asset paths
         #
         # See: http://hanamirb.org/guides/assets/digest
-        digest  true
+        # digest  true
 
         # Content Delivery Network (CDN)
         #
